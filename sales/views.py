63b88b828f80d4e74f5edfd6476fca6e9416e4c0
avjasso <avjasso@gmail.com>
2021-09-05 03:13:42 -0500

    Sale detail view, template and routes. Sales and positions dataframe

--- sales/views.py
+++ sales/views.py
@@ -1,15 +1,59 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from .models import Sale
+from .forms import SalesSearchForm
+import pandas as pd
 
 # Create your views here.
 
 
 def home_view(request):
-    hello = "hello world from the view"
-    return render(request, 'sales/home.html', {'hello': hello})
+    sales_df = None
+    positions_df = None
+    form = SalesSearchForm(request.POST or None)
+
+    if request.method == 'POST':
+        date_from = request.POST.get('date_from')
+        date_to = request.POST.get('date_to')
+        chart_type = request.POST.get('chart_type')
+
+        sale_qs = Sale.objects.filter(
+            created__date__lte=date_to, created__date__gte=date_from)
+        if len(sale_qs) > 0:
+            sales_df = pd.DataFrame(sale_qs.values())
+            positions_data = []
+            for sale in sale_qs:
+                for pos in sale.get_positions():
+                    obj = {
+                        'position_id': pos.id,
+                        'product': pos.product.name,
+                        'quantity': pos.quantity,
+                        'price': pos.price,
+                        'sales_id': pos.get_sales_id(),
+                    }
+                    positions_data.append(obj)
+
+            positions_df = pd.DataFrame(positions_data)
+
+            sales_df = sales_df.to_html()
+            positions_df = positions_df.to_html()
+        else:
+            pass
+
+    context = {
+        'form': form,
+        'sales_df': sales_df,
+        'positions_df': positions_df,
+    }
+
+    return render(request, 'sales/home.html', context)
 
 
 class SalesListView(ListView):
     model = Sale
     template_name = 'sales/main.html'
+
+
+class SaleDetailView(DetailView):
+    model = Sale
+    template_name = 'sales/detail.html'
